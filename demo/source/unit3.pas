1a3c5777e02232723cd8f7cb2fae5b0c64448d77
Fathony Luthfillah <x2nie@yahoo.com>
2015-12-28 12:40:40 +0700

    bugfix $ifdef begin $end end

--- demo/source/unit3.pas
+++ demo/source/unit3.pas
@@ -7,6 +7,7 @@ interface
 uses
   Classes, SysUtils, FileUtil, SynEdit, Forms, Controls,
   Graphics, Dialogs, ExtCtrls, ComCtrls, StdCtrls,
+  TypInfo,
   SynEditMarkupWordGroup,
   SynEditHighlighter,
   //SynHighlighterMiniPas2,//must before original
@@ -16,8 +17,10 @@ uses
   SynHighlighterPython,
   SynHighlighterHTML,
   SynHighlighterXML,
-  SynHighlighterJScript;
+  SynHighlighterJScript, SynEditTypes;
 
+const
+  MaxArray = 7;
 type
 
   TMarkupWordGroupAccess = class(TSynEditMarkupWordGroup)
@@ -32,6 +35,8 @@ type
     Panel1: TPanel;
     Panel2: TPanel;
     Panel3: TPanel;
+    StatusBar1: TStatusBar;
+    SynEdit1: TSynEdit;
     SynFreePascalSyn1: TSynFreePascalSyn;
     SynHTMLSyn1: TSynHTMLSyn;
     SynJScriptSyn1: TSynJScriptSyn;
@@ -47,11 +52,16 @@ type
     TabSheet7: TTabSheet;
     TabSheet8: TTabSheet;
     procedure FormCreate(Sender: TObject);
+    procedure SynEditStatusChange(Sender: TObject; Changes: TSynStatusChanges);
   private
     { private declarations }
-    FSyns : array[0..7] of TSynCustomHighlighter;
+    FSyns : array[0..MaxArray] of TSynCustomHighlighter;
+    FSynEdits : array[0..MaxArray] of TSynEdit;
     procedure CreateSynEdits;
     procedure LeaveOnly(ASynEdit:TSynEdit);
+    procedure CaretChanged(LogCaret: TPoint);
+  protected
+    function Current : integer;
   public
     { public declarations }
   end;
@@ -109,6 +119,14 @@ begin
 
 end;
 
+procedure TForm3.SynEditStatusChange(Sender: TObject;
+  Changes: TSynStatusChanges);
+begin
+
+  if [scCaretX, scCaretY] * Changes <> [] then
+    CaretChanged(FSynEdits[Current].LogicalCaretXY );
+end;
+
 
 
 
@@ -118,6 +136,7 @@ var i : integer;
     S : TSynEdit;
     Mi : TSynEditMarkupIfDef;
   F : array[0..7] of string = ('pas', 'pas', 'lfm', 'fold', 'bracket', 'py', 'xml', 'js');
+  Pan : TPanel;
 begin
   FSyns[0] := SynFreePascalSyn1;
   FSyns[1] := OriSynHighlighterPas.TSynFreePascalSyn.Create(self);
@@ -130,12 +149,15 @@ begin
 
   for i := 0 to Pred(self.PageControl1.PageCount) do
   begin
-    S := TSynEdit.Create(self);
+    FSynEdits[i] := TSynEdit.Create(self);
+    s := FSynEdits[i];
     s.Parent := PageControl1.Page[i];
     s.Align:= alClient;
     S.Lines.LoadFromFile('demo.'+ F[i]);
     s.Highlighter := FSyns[i];
     S.LineHighlightColor.Background:=panel1.Color;
+
+    S.OnStatusChange:= @SynEditStatusChange;
 
 
     M := TSynEditMarkupFoldColors.Create(S);
@@ -155,6 +177,15 @@ begin
       end;}
 
     end;
+    //
+    Pan := TPanel.Create(self);
+    Pan.Parent := PageControl1.Page[i];
+    Pan.Height := 32;
+    Pan.Align:=alTop;
+    Pan.Alignment:= taLeftJustify;
+    Pan.BevelOuter:= bvNone;
+    Pan.BevelInner:= bvLowered;
+    Pan.BorderWidth:=3;
   end;
 
 end;
@@ -176,6 +207,60 @@ begin
     end;
   end;
   PageControl1.ActivePage := TTabSheet(ASynEdit.Parent);
+end;
+
+procedure TForm3.CaretChanged(LogCaret: TPoint);
+  procedure sfa();
+  var
+    hl : TSynCustomFoldHighlighter;
+    TmpNode: TSynFoldNodeInfo;
+    NodeList: TLazSynFoldNodeInfoList;
+    y,i,c : integer;
+  begin
+    if not (FSyns[Current] is TSynCustomFoldHighlighter) then exit;
+    hl := TSynCustomFoldHighlighter(FSyns[Current]);
+    y := LogCaret.Y - 1;
+    //LCnt := Lines.Count;
+    //HL.CurrentLines := Lines;
+    HL.FoldNodeInfo[y].ClearFilter; // only needed once, in case the line was already used
+
+    (* Find the node under caret.
+       - for "end" (Procedure and begin) this is the inner (end of begin) node
+       - For "{$Else}" this is the closing node
+    *)
+    i := 0;
+    NodeList := HL.FoldNodeInfo[y];
+    NodeList.AddReference;
+    c := NodeList.Count;
+    try
+      NodeList.ActionFilter := [];// [sfaMarkup];
+      TmpNode := NodeList[i];
+      while (i < c) and not(sfaInvalid in TmpNode.FoldAction) and (TmpNode.LogXEnd < LogCaret.X-1) do
+      begin
+        inc(i);
+        TmpNode := NodeList[i];
+      end;
+      if (TmpNode.LogXStart > LogCaret.X - 1) or (sfaInvalid in TmpNode.FoldAction) then begin
+        StatusBar1.Panels[1].Text := '-';
+        StatusBar1.Panels[2].Text := '-';
+        exit;
+      end;
+
+      StatusBar1.Panels[1].Text := inttostr(TmpNode.FoldGroup);
+      StatusBar1.Panels[2].Text := SetToString(PTypeInfo(TypeInfo(TSynFoldActions)), integer( TmpNode.FoldAction), true);
+
+    finally
+      NodeList.ReleaseReference;
+    end;
+  end;
+begin
+  StatusBar1.Panels[0].Text := format(' %d, %d',[LogCaret.x, LogCaret.y]);
+  sfa();
+end;
+
+function TForm3.Current: integer;
+begin
+  result :=  PageControl1.ActivePageIndex;
 end;
 
 
